310b7a7af48b48871e996deb089795bc520fd478
idkvarg <idkvarg@outlook.com>
2025-09-09 15:37:36 +0330

    ...

--- handlers.py
+++ handlers.py
@@ -58,7 +58,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     for result in results:
         title = result.get("title") or result.get("trackName") or "Unknown"
         artist = result.get("uploader") or result.get("artistName") or ""
-        del result['thumbnails']
+        result['thumbnails'] = ''
         print(result)
         buttons.append([InlineKeyboardButton(f"{title} - {artist}", callback_data=cb_make("info", result['url']))])
 
