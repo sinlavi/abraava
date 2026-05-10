d66d901b7bc88023b7740c83d000955e30b5683d
Sina Alavi <atom-jogger-freely@duck.com>
2026-05-10 19:39:07 +0330

    Update itunes.py

--- crawlers/itunes.py
+++ crawlers/itunes.py
@@ -11,7 +11,7 @@ async def fetch_itunes(endpoint: str, params: dict) -> Optional[Dict[str, Any]]:
     session = await HttpClient.get_session()
     url = f"{ITUNES_BASE_URL}/{endpoint}"
     try:
-        async with session.get(url, params=params, ssl=False,proxy="http://127.0.0.1:8085") as resp:
+        async with session.get(url, params=params, ssl=False) as resp:
             if resp.status == 200:
                 text = await resp.text()
                 try:
